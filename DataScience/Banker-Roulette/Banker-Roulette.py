e9050b33d5b95c1098ae3be93068fe0095bcc372
Anamika <153952662+AnamikaVis@users.noreply.github.com>
2023-12-27 23:41:47 +0530

    Update Banker-Roulette.py

--- DataScience/Banker-Roulette/Banker-Roulette.py
+++ DataScience/Banker-Roulette/Banker-Roulette.py
@@ -1,1 +1,6 @@
-
+names = names_string.split(", ")
+import random
+num_items = len(names)
+random_choice = random.randint(0, num_items - 1)
+person_will_pay = names[random_choice]
+print( person_will_pay + " is going to buy the meal today!")
